b5d870f19b2afddbc95d13af7e08b2cc7d0275f1
Julien Ruaux <jruaux@c64>
2022-10-02 15:47:28 -0700

    fix: Made run script more resilient

--- run.sh
+++ run.sh
@@ -34,9 +34,9 @@ function test_systems_available {
   COUNTER=0
   until $(curl --output /dev/null --silent --head --fail http://localhost:$1); do
       printf '.'
-      sleep 2
+      sleep 10
       (( COUNTER+=1 ))
-      if [[ $COUNTER -gt 30 ]]; then
+      if [[ $COUNTER -gt 50 ]]; then
         MSG="\nWARNING: Could not reach configured kafka system on http://localhost:$1 \nNote: This script requires curl.\n"
 
           if [[ "$OSTYPE" == "darwin"* ]]; then
